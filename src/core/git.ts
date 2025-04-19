df6b8a18d4c55993c2f6df8d498cc1fdd4e712f6
mixturegg <its.mixture@gmail.com>
2025-04-19 19:00:24 +0500

    add: git diff

---
+++ src/core/git.ts
@@ -0,0 +1,24 @@
+import simpleGit from 'simple-git';
+import fs from 'fs';
+import path from 'path';
+
+export async function getGitDiff(useAllDiff: boolean = false): Promise<string> {
+  const git = simpleGit();
+
+  try {
+    const diff = useAllDiff ? await git.diff() : await git.diff(['--cached']);
+
+    const tempDir = path.resolve(process.cwd(), 'files');
+    if (!fs.existsSync(tempDir)) {
+      fs.mkdirSync(tempDir);
+    }
+
+    const filePath = path.join(tempDir, 'diff.txt');
+    fs.writeFileSync(filePath, diff, 'utf-8');
+
+    return diff;
+  } catch (error) {
+    console.error('❌ Failed in file git.ts:', error);
+    return '';
+  }
+}
